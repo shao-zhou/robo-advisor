99b513e20fc0e3fb56ed21b034dc15d63c3ecafd
shao-zhou <sz745@stern.nyu.edu>
2019-06-16 21:48:08 -0400

    Recent high price

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -2,6 +2,7 @@
 
 import requests
 import json 
+import time
 
 def to_usd(my_price):
     return "${0:,.2f}".format(my_price)
@@ -26,6 +27,17 @@ latest_day = dates[0] #assume first day is top; to do: sort to ensure latest day
 latest_close = tsd[latest_day]["4. close"]
 
 
+# get high price from each day
+# high_prices = [10, 20, 30, 5] # maximum of all high prices
+# recent_high = max(high_prices)
+
+high_prices = []
+
+for date in dates:
+    high_price = tsd[latest_day]["2. high"]
+    high_prices.append(float(high_price))
+
+recent_high = max(high_prices)
 
 #
 # INFO OUTPUTS
@@ -34,17 +46,24 @@ latest_close = tsd[latest_day]["4. close"]
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
+
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+
+now = time.strftime("%Y-%m-%d %H:%M:%p")
+# used code to format date and time suggested in this stackoverflow thread: https://stackoverflow.com/questions/31955761/converting-python-string-to-datetime-obj-with-am-pm
+print("REQUEST AT: " + str(now))
 print("-------------------------")
+
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
-print("RECENT HIGH: $101,000.00")
+print(f"RECENT HIGH: {to_usd(float(recent_high))}")
 print("RECENT LOW: $99,000.00")
+
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
+
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
