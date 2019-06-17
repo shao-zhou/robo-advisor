05fad889be87012f072057ce3852dfb05160dd51
shao-zhou <sz745@stern.nyu.edu>
2019-06-16 21:51:43 -0400

    Recent low price

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -32,12 +32,16 @@ latest_close = tsd[latest_day]["4. close"]
 # recent_high = max(high_prices)
 
 high_prices = []
+low_prices = []
 
 for date in dates:
     high_price = tsd[latest_day]["2. high"]
+    low_price = tsd[latest_day]["3. low"]
     high_prices.append(float(high_price))
+    low_prices.append(float(low_price))
 
 recent_high = max(high_prices)
+recent_low = min(low_prices)
 
 #
 # INFO OUTPUTS
@@ -58,7 +62,7 @@ print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print(f"RECENT HIGH: {to_usd(float(recent_high))}")
-print("RECENT LOW: $99,000.00")
+print(f"RECENT LOW: {to_usd(float(recent_low))}")
 
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
