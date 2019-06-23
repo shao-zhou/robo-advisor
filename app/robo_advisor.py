ea9bd5d96b5ab56fe1908401839ee6932034d95b
shao-zhou <sz745@stern.nyu.edu>
2019-06-23 10:47:21 -0400

    Check for alphabetic string input

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -22,10 +22,16 @@ api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
 try:
     symbol = input("Please specify a stock symbol (e.g. MSFT) and press enter: ")
 
+    if not symbol.isalpha():
+            print("Oh, expecting a properly-formed stock symbol like 'MSFT'. Please try again.")
+            exit()
+# used isalpha() to check for alphabetical input only: https://www.geeksforgeeks.org/python-string-isalpha-application/
+
     request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
     response = requests.get(request_url)
     parsed_response = json.loads(response.text)  # parse json into a dictionary
     last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
 # print(type(response))  #this is a Response
 # print(response.status_code)  #200
 # print(response.text)
